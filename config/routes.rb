304e564b1699a46e67164fe9cfe3225b4af505ac
romnest <roma.nest96@gmail.com>
2017-03-26 01:23:50 +0200

    Finished locale

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+
+  get 'set_language/english'
+
+  get 'set_language/ukraine'
+
   mount RailsAdmin::Engine => '//company/dashboard/admin', as: 'rails_admin'
+  
   get "/:page" => "pages#show"
+    
+  root "pages#show", page: "home"
 
   scope '/company' do
     resources :vacancies
@@ -13,6 +21,5 @@ Rails.application.routes.draw do
   scope '/company/' do
     resources :events
   end
-
-  root "pages#show", page: "home"
+    
 end
