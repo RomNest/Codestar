a0f6910847f16da726dcb977de55ba7c85403077
romnest <roma.nest96@gmail.com>
2017-03-26 02:30:46 +0200

    Finished contacts callback

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,20 @@
 class ContactsController < ApplicationController
+	def new
+		@contact = Contact.new
+	end
+
+	def create
+		@contact = Contact.new(contact_params)
+		
+		if @contact.save
+			redirect_to "/company/contacts"
+		else
+			redirect_to "/company/contacts"
+		end
+	end
+
+	private
+		def contact_params
+			params.require(:contact).permit(:name, :email, :subject, :message)
+		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,14 @@ Rails.application.routes.draw do
 
   get 'set_language/ukraine'
 
-  mount RailsAdmin::Engine => '//company/dashboard/admin', as: 'rails_admin'
+  mount RailsAdmin::Engine => '/company/dashboard/admin', as: 'rails_admin'
   
   get "/:page" => "pages#show"
-    
+  
+  resources "contacts", only: [:new, :create]
+  get "/company/contacts" => "contacts#new"
+
+
   root "pages#show", page: "home"
 
   scope '/company' do
